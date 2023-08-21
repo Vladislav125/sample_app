0c204fcc7c71b85a0b6e4afd9bb7d788be28daf7
Vladislav <myasoedovvladislav125@yandex.ru>
2023-08-21 16:24:00 +1000

    Login form added, but not working yet

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
